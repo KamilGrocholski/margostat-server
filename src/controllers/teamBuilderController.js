4a0ec43f648b7c154033828ae4b47d476c0eac88
Kazrek <mikololo26@interia.pl>
2022-08-23 02:14:23 +0200

    returny zmienione w teamBuilder

--- src/controllers/teamBuilderController.js
+++ src/controllers/teamBuilderController.js
@@ -3,8 +3,14 @@ import scrapClan from "../utils/puppeteer/teamBuilderScraper/index.js"
 export const getClan = async (req, res) => {
     const link = req.params.link
 
-    if (!link) return res.status(501).json({ status: 'Error', msg: 'Brak odnośnika do klanu.' })
+    if (!link) return res.status(401).json({ status: 'Error', msg: 'Brak odnośnika do strony klanowej.' })
 
-    const clanCharacters = await scrapClan(link)
-    return res.status(201).json({ clanCharacters })
+    try {
+        const clanCharacters = await scrapClan(link)
+        if (!clanCharacters) return res.status(501).json({ status: 'Error', msg: 'Nieprawdiłowy odnośnik do strony klanowej.' })
+        return res.status(201).json({ clanCharacters, status: 'Success' })
+    } catch (err) {
+        console.log(err)
+        return res.status(501).json({ status: 'Error', msg: 'Nieprawdiłowy odnośnik do strony klanowej.' })
+    }
 }
