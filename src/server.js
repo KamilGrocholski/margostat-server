6e8a475bd536db2dca5aa0c4d104b581647b9632
Kazrek <mikololo26@interia.pl>
2022-09-03 15:18:47 +0200

    extended; true

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 
 //Routes
 app.use('/api/statistics', statisticsRoute)
